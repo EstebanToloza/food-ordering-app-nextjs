5b1c3e186766bad725261aa0a64789572010d0c3
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-02-22 21:28:41 -0300

    papypal integration. unfinished

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,20 @@
+import dbConnect from "../../../utils/mongodb";
+import Product from "../../../models/Product";
+
+const handler = async (req, res) => {
+    const { method, query: {id} } = req;
+
+    if (method === "GET") {
+
+    };
+
+    if (method === "PUT") {
+        
+    };
+
+    if (method === "DELETE") {
+        
+    };
+}
+
+export default handler;
---
+++ pages/api/orders/index.js
@@ -0,0 +1,28 @@
+import dbConnect from "../../../utils/mongodb";
+import Product from "../../../models/Product";
+
+const handler = async (req, res) => {
+    const {method} = req;
+
+    await dbConnect();
+
+    if (method === "GET") {
+        try {
+            const order = await Order.find();
+            res.status(200).json(order)
+        } catch (error) {
+            res.status(500).json(err)
+        }
+    };
+
+    if (method === "POST") {
+        try {
+            const order = await Order.create(req.body);
+            res.status(201).json(order)
+        } catch (error) {
+            res.status(500).json(err)
+        }
+    };
+};
+
+export default handler;
